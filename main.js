41eb5cb346384edf4baaed28dc428669faba8543
Pinaki Priyadarsini Swain <81481960+Pinaki-priya-2021@users.noreply.github.com>
2021-06-30 13:45:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function hw_over(){
+    let hw=document.getElementById("hw");
+        hw.src="./wings-images/hw-desc.jpg";
+}
+function hw_out(){
+    let hw=document.getElementById("hw");
+    hw.src="./wings-images/hw.jpg";
+}
+function sw_over(){
+    let sw=document.getElementById("sw");
+    sw.src="./wings-images/sw-desc.jpg";
+}
+function sw_out(){
+    let sw=document.getElementById("sw");
+    sw.src="./wings-images/sw.jpg";
+}
+function design_over(){
+    let design=document.getElementById("design");
+    design.src="./wings-images/design-desc.jpg";
+
+}
+function design_out(){
+    let design=document.getElementById("design");
+    design.src="./wings-images/design.jpg";
+}
